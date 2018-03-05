d9a4cdfebdc54ae43b3ee83193a57050b52dea1b
Harevost <onlybest25@hotmail.com>
2018-03-05 16:10:40 +0800

    Factory mode on create app

--- manage.py
+++ manage.py
@@ -1,14 +1,17 @@
 """
 Vergermiya lightblog - script.py
 """
-
+import os
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-from vergermiya_lightblog.models import db, User, Post, Comment, Tag
-from vergermiya_lightblog import app
+from vergermiya_lightblog import create_app
+from vergermiya_lightblog import models
 
+
+env = os.environ.get("BLOG_ENV", "dev")
+app = create_app("vergermiya_lightblog.config.%sConfig" % env.capitalize())
 manager = Manager(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, models.db)
 
 manager.add_command("server", Server(host="0.0.0.0", port=80))
 manager.add_command("db", MigrateCommand)
@@ -20,9 +23,10 @@ def make_shell_context():
     create a python CLI
     :return: Default import project
     """
-    return dict(app=app, db=db,
-                User=User, Post=Post,
-                Comment=Comment, Tag=Tag)
+    return dict(app=app, db=models.db,
+                User=models.User, Post=models.Post,
+                Comment=models.Comment, Tag=models.Tag,
+                Server=Server)
 
 
 if __name__ == '__main__':
